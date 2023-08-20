44d888fe2071d0cf383af112cd882838dee01292
Pavel Urbanovich <yrbik@mail.ru>
2023-08-20 13:53:57 +0300

    feat: add project files

---
+++ library-app/src/App.js
@@ -0,0 +1,37 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      {/* <header className="App-header">
+        <div className='Container'>
+          <div className='Header-container'>
+            <div className='Header-name'>
+            Brooklyn Public Library
+            </div>
+          </div>
+        </div>
+
+
+
+
+        {<img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p> */}
+        {/* { <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> }
+      </header>} */}
+    </div>
+  );
+}
+
+export default App;
---
+++ library-app/src/react-module/Header.js
@@ -0,0 +1,52 @@
+import './Header.css'
+
+function Header() {
+  const menuArr = [
+    <li className='Link-item' key={1}>
+      <a href="google.com" className='Link-item-list'>
+        About
+      </a>
+    </li>,
+    <li className='Link-item' key={2}>
+      <a href="google.com" className='Link-item-list'>
+        Favorites
+      </a>
+    </li>,
+    <li className='Link-item' key={3}>
+      <a href="google.com" className='Link-item-list'>
+        Coffee shop
+      </a>
+    </li>,
+    <li className='Link-item' key={4}>
+    <a href="google.com" className='Link-item-list'>
+      Contacts
+    </a>
+    </li>,
+    <li className='Link-item' key={5}>
+    <a href="google.com" className='Link-item-list'>
+      Library Card
+    </a>
+    </li>
+  ]
+
+
+  return (
+    <header className="App-header">
+      <div className='Container'>
+        <div className='Header-container'>
+          <div className='Header-name'>
+          Brooklyn Public Library
+          </div>
+          <nav className='Navigation'>
+            <ul className='Menu'>
+              {menuArr}
+            </ul>
+          </nav>
+
+        </div>
+      </div>
+    </header>
+    );
+  }
+
+  export default Header;
