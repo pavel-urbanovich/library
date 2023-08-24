d9d5c0b90b9bc830cba5e4937cc8ba97317e4dc6
Pavel Urbanovich <yrbik@mail.ru>
2023-08-24 17:49:42 +0300

    fix: section welcom

---
+++ library-app/src/components/Main.js
@@ -0,0 +1,20 @@
+
+function AddMain() {
+  return (
+    <main className='Main'>
+      <section className='Welcome-section'>
+          <div className='Title-container'>
+            <h1 className='Title'>
+            Welcome
+              <div>to the Brooklyn Library
+              </div>
+          </h1>
+          </div>
+
+      </section>
+
+    </main>
+);
+}
+
+export default AddMain
--- library-app/src/index.js
+++ library-app/src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import './normalize.css';
+import './css/index.css';
+import './css/normalize.css';
+import './css/header.css'
+import './css/main.css'
 import reportWebVitals from './reportWebVitals';
-import Header from './react-module/Header';
+import Header from './components/Header';
+import AddMain from './components/Main';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
     <Header />
+    <AddMain />
   </React.StrictMode>
 );
 
